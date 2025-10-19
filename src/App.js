e3ea29a60eea5ea506c7bef5c90a927ee6e19fa5
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2025-10-19 19:07:44 +0900

    feat(calculator): 커스텀 구분자 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ class App {
             const input = await Console.readLineAsync(
                 "덧셈할 문자열을 입력해 주세요.\n"
             );
+    calculate(input) {
+        let numbers = [];
+
+        if (input.startsWith("//")) {
+            const [_, custom, rest] = input.match(/^\/\/(.)\\n(.*)$/) || [];
+            }
+            numbers = rest.split(custom);
         } else {
             numbers = input.split(/[,|:]/);
         }

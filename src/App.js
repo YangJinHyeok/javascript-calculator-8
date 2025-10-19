8f4708e5ec1b2c5d7a511b61dba7028a40ba023a
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2025-10-19 19:09:58 +0900

    feat(output): 결과 출력 및 빈 문자열 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App {
             const input = await Console.readLineAsync(
                 "덧셈할 문자열을 입력해 주세요.\n"
             );
+            const result = this.calculate(input);
+            Console.print(`결과 : ${result}`);
         } catch (error) {
             if (error instanceof Error && error.message.startsWith("[ERROR]")) {
                 throw error;
@@ -14,6 +16,8 @@ class App {
     }
 
     calculate(input) {
+        if (!input || input.trim() === "") return 0;
+
         let numbers = [];
 
         if (input.startsWith("//")) {

3154c9e29fb7b1fc1435595f46ecce2955272842
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2025-10-19 19:06:49 +0900

    feat(parser): 기본 구분자(쉼표, 콜론)를 기준으로 문자열 분리 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App {
             const input = await Console.readLineAsync(
                 "덧셈할 문자열을 입력해 주세요.\n"
             );
+        } else {
+            numbers = input.split(/[,|:]/);
+        }
 }
 
 export default App;
